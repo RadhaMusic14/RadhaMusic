ea7a35356b394a0f28bbc33b2d2d0ec83d5a55c6
RadhaMusic14 <radhaxmusic@gmail.com>
2025-02-08 13:01:41 +0530

    ᴅᴏɴᴇ ✓

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from RADHAMUSIC.core.bot import VISHNU
+from RADHAMUSIC.core.dir import dirr
+from from RADHAMUSIC.core.git import git
+from RADHAMUSIC.core.userbot import Userbot
+from RADHAMUSIC.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = VISHNU()
+userbot = Userbot()
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
