3e0d034f7c44d2af9f76d0106525dbc1351e0b11
MrDevloaper <MrDevloaper@gmail.com>
2025-02-08 17:00:40 +0530

    ᴅᴏɴᴇ ✓

--- RADHAMUSIC/utils/__init__.py
+++ RADHAMUSIC/utils/__init__.py
@@ -1,26 +1,9 @@
-
-from RADHAMUSIC.core.bot import VISHNU
-from RADHAMUSIC.core.dir import dirr
-from RADHAMUSIC.core.git import git
-from RADHAMUSIC.core.userbot import Userbot
-from RADHAMUSIC.misc import dbb, heroku
-
-from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-
-app = VISHNU()
-userbot = Userbot()
-
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .extraction import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
+from .imposterdb import *
